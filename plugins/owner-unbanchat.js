a5da42f03f35024ea9bc0cc82c06ba28379bf373
hotarooob <127650971+hotarooob@users.noreply.github.com>
2024-04-21 16:37:35 +0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,10 +3,10 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner} ) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('✅Tʜᴇ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ᴀᴄᴛɪᴠᴀᴛᴇᴅ ғᴏʀ ᴛʜɪs ᴄʜᴀᴛ𒁂')   
+    m.reply('✅تم اعاده تفعيل البوت لهذه المجموعة𒁂')   
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = ['boton', 'unbanchat'] 
+handler.command = ['تفعيل-المجموعة'] 
 
 export default handler
