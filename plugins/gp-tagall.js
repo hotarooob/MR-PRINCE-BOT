6364b8de6bf40ff45c2393f8d54b5cacf4485c5d
hotarooob <127650971+hotarooob@users.noreply.github.com>
2024-04-21 16:31:22 +0300

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    m.reply(`▢ Group : *${groupMetadata.subject}*\n▢ Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n└──✪ 𝙿𝚛𝚒𝚗𝚌𝚎 ┃ ᴮᴼᵀ ✪──', null, {
-        mentions: users
-    })
+  let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+  m.reply(`▢ المجموعة : *${groupMetadata.subject}*\n▢ الاعضاء : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n\n•━━━━•🦇•━━━━•\n\n` + users.map(v => '▢ @' + v.replace(/@.+/, '')).join`\n` + '\n\n•━━━━•🦇•━━━━•', null, {
+      mentions: users
+  })
 }
 
 handler.help = ['tagall']
 handler.tags = ['group']
-handler.command = ['tagall']
+handler.command = ['منشن']
 handler.admin = true
 handler.group = true
 
